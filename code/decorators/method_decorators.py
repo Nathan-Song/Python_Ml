031946c32c3f4f67cab9f005a9a066feec2941ee
franznkemaka <franz@rigle.co>
2024-09-05 11:18:48 +0200

    🚀

---
+++ code/decorators/method_decorators.py
@@ -0,0 +1,17 @@
+
+
+def get_user():
+    process_get_request()
+    print("Get user")
+    send_get_response()
+
+
+def process_get_request():
+    print("Get the params")
+
+
+def send_get_response():
+    print("Send the response to user")
+
+
+get_user()
